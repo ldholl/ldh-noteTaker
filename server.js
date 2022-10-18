5bbc35426a8e96a4b997b997e37e9c1bfe767284
ldholl <lacy.d.holleman@gmail.com>
2022-10-18 10:18:18 -0400

    added PORT variation

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const [ notes ] = require('./data/notes.json');
 
+
+//ROUTES
+//API Routes
 app.get('/api/notes', (req, res) => {
     res.json(notes)
 })
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+//POST /api/notes to receive new note to save on the body
+
+
+//HTML Routes
+//GET * to return index.html
+//GET /notes to return notes.html
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
