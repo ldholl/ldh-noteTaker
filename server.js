7339dfaac8a5df38ab9cc47c16963b9b8d518f7b
ldholl <lacy.d.holleman@gmail.com>
2022-10-18 11:30:51 -0400

    added note creation function and validation

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const [ notes ] = require('./data/notes.json');
+const { notes } = require('./data/notes.json');
 
 
 //ROUTES
@@ -12,13 +17,46 @@ app.get('/api/notes', (req, res) => {
 })
 
 //POST /api/notes to receive new note to save on the body
+app.post('/api/notes', (req, res) => {
+    //set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
 
+    //validate
+    if(!validateNote(req.body)){
+        res.status(400).send('The note is not properly formatted.')
+    } else {
+        //add note to json file and notes array
+        const note = createNewNote(req.body, notes);
+
+        res.json(note)
+    }    
+});
 
 //HTML Routes
 //GET * to return index.html
 //GET /notes to return notes.html
 
+//FUNCTIONS
+function createNewNote(body, notesArray){
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './data/notes.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
 
+    return note;
+   
+}
+
+function validateNote(note){
+    if(!note.title || typeof note.title !== 'string'){
+        return false;
+    }
+    if(!note.text || typeof note.text !== 'string'){
+        return false;
+    }
+}
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
